3563155fd56912bd0dc196df2035ff1bca1d4c2a
shani1 <shani@nimapinfotech.com>
2023-02-28 11:49:20 +0530

    site setting changes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {RouterStateService} from "../../../core/services/router-state.service";
-import {DestroyableComponent} from "../destroyable/destroyable.component";
-import {Observable, takeUntil} from "rxjs";
-import {AuthService} from "../../../core/services/auth.service";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterStateService } from '../../../core/services/router-state.service';
+import { DestroyableComponent } from '../destroyable/destroyable.component';
+import { BehaviorSubject, Observable, takeUntil } from 'rxjs';
+import { AuthService } from '../../../core/services/auth.service';
+import { LandingService } from 'src/app/core/services/landing.service';
 
 @Component({
   selector: 'dfm-header',
@@ -18,24 +19,36 @@ export class HeaderComponent extends DestroyableComponent implements OnInit, OnD
     {
       name: 'NL',
       value: 'NL',
-    }
+    },
   ];
+
+  public base64Start = 'data:image/jpeg;base64,';
 
   public url!: string;
 
   public isLoggedIn$!: Observable<boolean>;
 
-  constructor(private routerStateSvc: RouterStateService, private authSvc: AuthService) {
+  public siteDetails$$: BehaviorSubject<any[]>;
+
+  constructor(private routerStateSvc: RouterStateService, private authSvc: AuthService, private landingService: LandingService) {
     super();
+    this.siteDetails$$ = new BehaviorSubject<any[]>([]);
   }
 
   public ngOnInit(): void {
+    this.landingService.siteFooterDetails$$.pipe(takeUntil(this.destroy$$)).subscribe((res) => {
+      console.log('appointments upcomming: ', res);
+      this.siteDetails$$.next(res);
+    });
+
     this.isLoggedIn$ = this.authSvc.isLoggedIn$;
-    this.routerStateSvc.listenForUrlChange$().pipe(takeUntil(this.destroy$$)).subscribe((url) => this.url = url);
+    this.routerStateSvc
+      .listenForUrlChange$()
+      .pipe(takeUntil(this.destroy$$))
+      .subscribe((url) => (this.url = url));
   }
 
   public override ngOnDestroy() {
     super.ngOnDestroy();
   }
-
 }
