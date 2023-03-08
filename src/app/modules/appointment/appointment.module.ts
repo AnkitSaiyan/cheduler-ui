ed89e731b4f44e2a0c45597cb257f0ffa99b452a
shani1 <shani@nimapinfotech.com>
2023-03-08 23:05:16 +0530

    appointment page screen ui

--- src/app/modules/appointment/appointment.module.ts
+++ src/app/modules/appointment/appointment.module.ts
@@ -1,23 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppointmentComponent } from './pages/appointment/appointment.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AppointmentRoutingModule } from './appointment-routing.module';
 import { RouterModule } from '@angular/router';
+import { AppointmentComponent } from './pages/appointment/appointment.component';
+import { AppointmentRoutingModule } from './appointment-routing.module';
 import { ViewComponent } from './components/view/view.component';
 
-
-
 @NgModule({
-  declarations: [
-    AppointmentComponent,
-    ViewComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    AppointmentRoutingModule,
-    RouterModule
-  ]
+  declarations: [AppointmentComponent, ViewComponent],
+  imports: [CommonModule, SharedModule, AppointmentRoutingModule, RouterModule],
 })
-export class AppointmentModule { }
+export class AppointmentModule {}
