8e2f97a6a00734a0c2124796097b32ba85a21ec3
ankit.kumar <ankit.kumar@mindrops.com>
2023-03-30 14:43:55 +0530

    Loader added

--- src/app/core/services/schedule-appointment.service.ts
+++ src/app/core/services/schedule-appointment.service.ts
@@ -141,7 +141,8 @@ export class ScheduleAppointmentService {
   }
 
   private fetchAllPhysicians(): Observable<any[]> {
-    return this.http.get<BaseResponse<any[]>>(`${environment.serverBaseUrl}/doctor`).pipe(map((response) => response.data));
+    return this.http.get<BaseResponse<any[]>>(`${environment.serverBaseUrl}/doctor`).pipe(
+      map((response) => response.data))
   }
 
   public get exams$(): Observable<Exam[]> {
@@ -149,7 +150,8 @@ export class ScheduleAppointmentService {
   }
 
   private fetchAllExams(): Observable<Exam[]> {
-    return this.http.get<BaseResponse<Exam[]>>(`${environment.serverBaseUrl}/common/getexams`).pipe(map((response) => response.data));
+    this.loaderSvc.spinnerActivate()
+    return this.http.get<BaseResponse<Exam[]>>(`${environment.serverBaseUrl}/common/getexams`).pipe(map((response) => response.data), tap(() => this.loaderSvc.spinnerDeactivate()));
   }
 
   public getExamByID(examID: number): Observable<Exam | undefined> {
@@ -263,6 +265,15 @@ export class ScheduleAppointmentService {
     return ap;
   }
 }
+
+
+
+
+
+
+
+
+
 
 
 
