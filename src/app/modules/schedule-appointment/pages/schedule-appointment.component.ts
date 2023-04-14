b40f82cdf57c2961de1dbe15d1d361f8eb341518
ankit.kumar <ankit.kumar@mindrops.com>
2023-04-14 15:29:46 +0530

    Routing bug fixed

--- src/app/modules/schedule-appointment/pages/schedule-appointment.component.ts
+++ src/app/modules/schedule-appointment/pages/schedule-appointment.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterStateService } from '../../../core/services/router-state.service';
 import { DestroyableComponent } from '../../../shared/components/destroyable/destroyable.component';
-import { combineLatest, filter, pairwise, takeUntil } from 'rxjs';
+import { combineLatest, filter, pairwise, takeUntil, tap } from 'rxjs';
 import { ScheduleAppointmentService } from '../../../core/services/schedule-appointment.service';
 import { Router, RoutesRecognized } from '@angular/router';
 
@@ -29,25 +29,15 @@ export class ScheduleAppointmentComponent extends DestroyableComponent implement
   }
 
   public ngOnInit(): void {
-    this.router.events
-      .pipe(
-        filter((e) => e instanceof RoutesRecognized),
-        pairwise(),
-      )
-      .subscribe((event: any[]) => {
-        console.log(event[0].urlAfterRedirects);
-      });
-
-    combineLatest([this.routerStateSvc.listenForUrlChange$(), this.scheduleAppointmentSvc.basicDetails$])
+    combineLatest([this.routerStateSvc.listenForUrlChange$(), this.scheduleAppointmentSvc.examDetails$])
       .pipe(takeUntil(this.destroy$$))
-      .subscribe(([url, basicDetails]) => {
-        console.log(url.includes('confirm'), Object.keys(basicDetails).length, basicDetails);
-
-        if (url.includes('confirm') && !Object.keys(basicDetails).length) {
-          // this.router.navigate(['/', 'dashboard'], {
-          //   replaceUrl: true,
-          //   queryParams: null,
-          // });
+      .subscribe(([url, examDetails]) => {
+        console.log(url.includes('confirm'), Object.keys(examDetails).length, examDetails);
+        if (url.includes('confirm') && !Object.keys(examDetails).length) {
+          this.router.navigate(['/', 'dashboard'], {
+            replaceUrl: true,
+            queryParams: null,
+          });
         }
         this.url = url;
       });
