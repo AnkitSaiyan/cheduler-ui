0fa076432cbfd8015c730f4a98a43d067e0ffe8c
ankit.kumar <ankit.kumar@mindrops.com>
2023-04-19 16:55:37 +0530

    patient - add a pipe for UTC to local

---
+++ src/app/shared/pipes/utc-to-local.pipe.ts
@@ -0,0 +1,54 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dfmUtcToLocal',
+})
+export class UtcToLocalPipe implements PipeTransform {
+  public transform(utcDateTimeString: string, timeOnly = false): string {
+    if (!utcDateTimeString) {
+      return utcDateTimeString;
+    }
+
+    if (timeOnly) {
+      return this.utcTimeToLocalTimeString(utcDateTimeString);
+    }
+
+    return this.utcDateToLocalDate(new Date(utcDateTimeString)).toString();
+  }
+
+  private utcDateToLocalDate(utcDate: Date): Date {
+    if (!utcDate) {
+      return utcDate;
+    }
+
+    const newDate = new Date();
+    newDate.setTime(utcDate.getTime() - utcDate.getTimezoneOffset() * 60 * 1000);
+
+    return newDate;
+  }
+
+  private utcTimeToLocalTimeString(timeString: string): string {
+    if (timeString.split(':').length < 2) {
+      return timeString;
+    }
+
+    // generating utc date from time string
+    const hour = +timeString.split(':')[0];
+    const min = +timeString.split(':')[1];
+    const utcDate = new Date();
+    utcDate.setHours(hour);
+    utcDate.setMinutes(min);
+
+    // getting local date and time zone difference offset in minutes
+    const localDate = new Date();
+    const offsetMinutes = localDate.getTimezoneOffset();
+    localDate.setTime(utcDate.getTime() - offsetMinutes * 60 * 1000);
+
+    const localHour = `${localDate.getHours()}0`.slice(0, 2);
+
+    const localMin = `${localDate.getMinutes()}0`.slice(0, 2);
+
+    return `${localHour}:${localMin}`;
+  }
+}
+
