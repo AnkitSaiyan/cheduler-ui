810654529cb85eb782ba5fd993b48fd316c6f492
Ankit Kumar <ankit.kumar@mindrops.com>
2023-06-23 17:31:10 +0530

    Url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
   production: false,
-  serverBaseUrl: 'https://diflexmo-scheduler-api-uat.azurewebsites.net/api',
+  serverBaseUrl: 'https://diflexmo-scheduler-api-dev.azurewebsites.net/api',
   userManagementApiUrl: 'https://auth.diflexmo.be/usermanagement/api',
   authClientId: 'd526e147-4713-4a0a-bf56-d8f500fb9a62',
   redirectUrl: window.location.origin,
