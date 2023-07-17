5447bebca06227fc2d4b608f2637e5c4be246a33
ankit.kumar <ankit.kumar@mindrops.com>
2023-07-17 19:07:49 +0530

    body female front and back popover complete

--- src/app/modules/schedule-appointment/components/exam-detail/models/organ-female-back-model/organ-female-back-model.component.ts
+++ src/app/modules/schedule-appointment/components/exam-detail/models/organ-female-back-model/organ-female-back-model.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ExamService } from 'src/app/core/services/exam.service';
+import { AnatomyMatMenu } from 'src/app/shared/components/anatomy-mat-menu/anatomy-mat-menu';
+import { BodyFemaleBack } from 'src/app/shared/utils/anatomy.enum';
 
 @Component({
   selector: 'dfm-organ-female-back-model',
   templateUrl: './organ-female-back-model.component.html',
   styleUrls: ['./organ-female-back-model.component.scss'],
 })
-export class OrganFemaleBackModelComponent implements OnInit {
-  constructor(public examSvc: ExamService) {}
+export class OrganFemaleBackModelComponent extends AnatomyMatMenu<BodyFemaleBack> implements OnDestroy {
+  public category = BodyFemaleBack;
+  constructor(public examSvc: ExamService) {
+    super();
+  }
 
-  ngOnInit() {}
+  public override ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
 }
-
 
--- src/app/modules/schedule-appointment/components/exam-detail/models/organ-female-front-model/organ-female-front-model.component.ts
+++ src/app/modules/schedule-appointment/components/exam-detail/models/organ-female-front-model/organ-female-front-model.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ExamService } from 'src/app/core/services/exam.service';
+import { AnatomyMatMenu } from 'src/app/shared/components/anatomy-mat-menu/anatomy-mat-menu';
+import { BodyFemaleFront } from 'src/app/shared/utils/anatomy.enum';
 
 @Component({
   selector: 'dfm-organ-female-front-model',
   templateUrl: './organ-female-front-model.component.html',
   styleUrls: ['./organ-female-front-model.component.scss'],
 })
-export class OrganFemaleFrontModelComponent implements OnInit {
-  constructor(public examSvc: ExamService) {}
+export class OrganFemaleFrontModelComponent extends AnatomyMatMenu<BodyFemaleFront> implements OnDestroy {
+  public category = BodyFemaleFront;
+  constructor(public examSvc: ExamService) {
+    super();
+  }
 
-  ngOnInit() {}
+  public override ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
 }
-
 
