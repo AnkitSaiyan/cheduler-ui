02d5db493dcfa38b50602a49efb0d75cda0f8396
sonu-mindrops <sonu@mindrops.com>
2023-08-29 12:09:25 +0530

    bug fix

--- src/app/modules/schedule-appointment/pages/schedule-appointment.component.ts
+++ src/app/modules/schedule-appointment/pages/schedule-appointment.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterStateService } from '../../../core/services/router-state.service';
 import { DestroyableComponent } from '../../../shared/components/destroyable/destroyable.component';
-import { combineLatest, filter, map, pairwise, takeUntil, tap } from 'rxjs';
+import { Observable, combineLatest, filter, map, pairwise, takeUntil, tap } from 'rxjs';
 import { ScheduleAppointmentService } from '../../../core/services/schedule-appointment.service';
 import { Router, RoutesRecognized } from '@angular/router';
 import { LandingService } from 'src/app/core/services/landing.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'dfm-schedule-appointment',
@@ -18,7 +19,11 @@ export class ScheduleAppointmentComponent extends DestroyableComponent implement
 
   public siteSetting: any;
 
-  constructor(private routerStateSvc: RouterStateService, private scheduleAppointmentSvc: ScheduleAppointmentService, private router: Router, private landingService: LandingService) {
+  public isLoggedIn$!: Observable<boolean>;
+
+  public userName$!: Observable<string | undefined>;
+
+  constructor(private routerStateSvc: RouterStateService, private scheduleAppointmentSvc: ScheduleAppointmentService, private router: Router, private landingService: LandingService, private authSvc: AuthService) {
     super();
 
     this.routerStateSvc
@@ -39,7 +44,10 @@ export class ScheduleAppointmentComponent extends DestroyableComponent implement
           this.siteSetting = details?.data;
           localStorage.setItem('siteDetails', JSON.stringify(details));
         }),
-      ).subscribe();
+    ).subscribe();
+    
+    this.isLoggedIn$ = this.authSvc.isLoggedIn$;
+    this.userName$ = this.authSvc.authUser$.pipe(map((user) => user?.displayName));
 
     combineLatest([this.routerStateSvc.listenForUrlChange$(), this.scheduleAppointmentSvc.examDetails$])
       .pipe(takeUntil(this.destroy$$))
